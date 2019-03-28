7650d386a2ee8f0983498abdc888f451eab0d88d
walayolo <as13750019770@gmail.com>
2019-03-28 08:23:15 +0800

    add demo

--- app/user/view.py
+++ app/user/view.py
@@ -9,6 +9,7 @@ from .forms import LoginForm, RegisterForm, PasswordResetRequestForm, ChangePass
 from . import user
 from .. import db
 from ..models import User
+from flask import jsonify
 
 @user.before_app_request
 def before_request():
@@ -32,13 +33,24 @@ def unconfirmed():
 @user.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        users = User.query.filter_by(email=form.email.data).first()
-        if users is not None and users.verify_password(form.password.data):
-            login_user(users, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash(u'用户名或者密码错误')
-    return render_template('user/login.html', form=form)
+    user = User.query.filter_by(email=form.email.data).first()
+    if user :
+        print user.email
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return 'ok'
+    return 'ok'
+
+@user.route('/logout', methods=['GET','POST'])
+def logout():
+    logout_user()
+    return 'ok'
+
+@user.route('/api/check',methods=['GET',])
+@login_required
+def api_check():
+    return  jsonify({'username':'高鹏','authcheck':'true','birthday':'1990.9.1',
+                        'statments':['测试用例一','测试用例二','测试用例三']})
 
 @user.route('/register',methods=['GET', 'POST'])
 def register():
